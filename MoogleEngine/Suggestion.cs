a52c6dcf166368fc34b0a599f20919ab73080842
Sergio0307 <129115608+Sergio0307@users.noreply.github.com>
2023-05-08 22:44:36 -0500

    Add files via upload

---
+++ MoogleEngine/Suggestion.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoogleEngine
+{
+    internal class Suggestion
+    {
+        public static int LevenshteinDistance(string str1, string str2)
+        {
+            int[,] matrix = new int[str1.Length + 1, str2.Length + 1];
+
+            for (int i = 0; i <= str1.Length; i++)
+            {
+                matrix[i, 0] = i;
+            }
+            for (int j = 0; j <= str2.Length; j++)
+            {
+                matrix[0, j] = j;
+            }
+            for (int i = 1; i <= str1.Length; i++)
+            {
+                for (int j = 1; j <= str2.Length; j++)
+                {
+                    int cost = (str1[i - 1] == str2[j - 1]) ? 0 : 1;
+
+                    matrix[i, j] = Math.Min(Math.Min(matrix[i - 1, j] + 1, matrix[i, j - 1] + 1), matrix[i - 1, j - 1] + cost);
+                }
+            }
+            return matrix[str1.Length, str2.Length];
+        }
+        public static string[] ConvertDocArray(string documento)
+        {
+            string[] palabras = documento.Split(' ',StringSplitOptions.RemoveEmptyEntries);
+            return palabras;
+        }
+
+    }
+
+}
